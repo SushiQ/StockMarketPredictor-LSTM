2f470e0c88813c393d8fe4c41619372b8a83179d
AxelDrotz <axeldr@kth.se>
2022-05-27 12:28:33 +0200

    update
    asdasf

--- predictor.py
+++ predictor.py
@@ -15,6 +15,7 @@ warnings.filterwarnings('ignore')
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import load_model
 from tensorflow.keras.layers import LSTM
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import Dropout
@@ -64,155 +65,57 @@ if os.path.exists('config.py'):
     print(1)
 else:
     print(0)
-
-
-oneFeature = False
-if oneFeature:
-    #keras only takes numpy array
-    size = dataset_train['Open'].size
-
-    training_set = dataset_train.iloc[:round(size*0.8), 1: 2].values
-
-    #scaler = MinMaxScaler(feature_range = (0, 1))
-    #fit: get min/max of train data
-    #training_set_scaled = scaler.fit_transform(training_set)
-
-
-    ## 60 timesteps and 1 output
-    X_train = []
-    y_train = []
-    for i in range(60, len(training_set_scaled)):
-        X_train.append(training_set_scaled[i-60: i, 0])
-        y_train.append(training_set_scaled[i, 0])
-
-    X_train, y_train = np.array(X_train), np.array(y_train)
-
-    print(X_train.shape)
-    print(y_train.shape)
-
-    # Adding a 1 to indicate the dimension of the input (only looking at 'Open')
-    # Number of stock prices - 1449
-    # Number of time steps - 60
-    # Number of Indicator - 1
-    X_train = np.reshape(X_train, newshape =
-                         (X_train.shape[0], X_train.shape[1], 1))
-
-    print(X_train.shape)
-
-    # Creating the architecture
-    regressor = Sequential()
-    #add 1st lstm layer: 512 neurons
-    regressor.add(LSTM(units = 512, return_sequences = True, input_shape = (X_train.shape[1], 1)))
-    regressor.add(Dropout(rate = 0.2))
-
-    ##add 2nd lstm layer: 512 neurons
-    regressor.add(LSTM(units = 512, return_sequences = True))
-    regressor.add(Dropout(rate = 0.2))
-
-    ##add 3rd lstm layer
-    regressor.add(LSTM(units = 32, activation = 'relu', return_sequences = False))
-    regressor.add(Dropout(rate = 0.2))
-
-    ##add output layer
-    regressor.add(Dense(units = 1))
-
-    regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
-    regressor.fit(x = X_train, y = y_train, batch_size = 512, epochs = 2)
-
-
-    # The test data set
-    real_stock_price = dataset_train.iloc[round(size*0.8):, 1: 2].values
-    real_stock_price.shape
-
-    dataset_total = dataset_train['Open']
-
-    ##use .values to make numpy array
-    inputs = dataset_total[len(training_set) - 60:].values
-
-
-    #reshape data to only have 1 col
-    inputs = inputs.reshape(-1, 1)
-    #scale input
-    inputs = scaler.transform(inputs)
-
-    # Create test data strucutre
-    X_test = []
-    for i in range(60, len(inputs)):
-        X_test.append(inputs[i-60:i, 0])
-    X_test = np.array(X_test)
-    #add dimension of indicator
-    X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
-    print("X_test.shape = ", X_test.shape)
-
-
-    predicted_stock_price = regressor.predict(X_test)
-
-    #inverse the scaled value
-    predicted_stock_price = scaler.inverse_transform(predicted_stock_price)
-
-
-    ##visualize the prediction and real price
-    plt.plot(real_stock_price, label = 'Real price')
-    plt.plot(predicted_stock_price, label = 'Predicted price')
-
-    plt.title('Price prediction')
-    plt.xlabel('Time')
-    plt.ylabel('Price')
-    plt.legend()
-    plt.show()
-
-
-else:
-    # Selecting 5 interesting feature
-    input_feature = dataset_train[['Open', 'High', 'Low', 'Volume', 'Close']]
+training = False
+oneFeature = True
+if training == False:
+    if oneFeature:
+        regressor = load_model("modeloneinput.h5")
+    input_feature = dataset_train[['Close']]
     input_data = input_feature.values
     print("type", type(input_data))
-    #scaler = MinMaxScaler(feature_range=(0, 1))
+    # scaler = MinMaxScaler(feature_range=(0, 1))
     # input_data[:,:] = scaler.fit_transform(input_data[:,:])
 
-    #input_data = scaler.fit_transform(input_data)
+    # input_data = scaler.fit_transform(input_data)
     # Normalize the data
     inputnew = []
     print("going to scale")
-    print("shape input" , input_data.shape)
+    print("shape input", input_data.shape)
     katt = 0
-    rangestuff =8
-    a = np.zeros((int((rangestuff+1)*len(input_data)/rangestuff),5))
+    rangestuff = 8
+    print(len(input_data))
+    a = np.zeros((len(input_data), 1))
     for i in range(rangestuff):
-
-        scaler = MinMaxScaler(feature_range=(0,1))
+        scaler = MinMaxScaler(feature_range=(0, 1))
         # input_data[:,:] = scaler.fit_transform(input_data[:,:])
 
-        t = scaler.fit_transform(input_data[int(i*len(input_data)/rangestuff):int((i+1)*len(input_data)/rangestuff),:])
-        a[int(i*len(input_data)/rangestuff):int((i+1)*len(input_data)/rangestuff),:] = t
+        t = scaler.fit_transform(
+            input_data[int(i * len(input_data) / rangestuff):int((i + 1) * len(input_data) / rangestuff), :])
+        a[int(i * len(input_data) / rangestuff):int((i + 1) * len(input_data) / rangestuff), :] = t
 
         inputnew.append(t)
 
     input_data = a
 
-
-
+    X = []
+    y = []
     lookback = 30
     total_size = len(dataset_train)
-
-    X=[]
-    y=[]
-    print("shape input after" , input_data.shape)
-    for i in range(0, total_size-lookback): # loop data set with margin 50 as we use 50 days data for prediction
-        t=[]
-        for j in range(0, lookback): # loop for 50 days
-            current_index = i+j
-            t.append(input_data[current_index, :]) # get data margin from 50 days with marging i
+    print("shape input after", input_data.shape)
+    for i in range(0, total_size - lookback):  # loop data set with margin 50 as we use 50 days data for prediction
+        t = []
+        for j in range(0, lookback):  # loop for 50 days
+            current_index = i + j
+            t.append(input_data[current_index, :])  # get data margin from 50 days with marging i
 
         # X 12580x 30x 5
         # t 30 x 5
 
         X.append(t)
 
+        y.append(input_data[lookback + i, 0])
 
-        y.append(input_data[lookback+i, 4])
-
-    X, y= np.array(X), np.array(y)
+    X, y = np.array(X), np.array(y)
     print("shapes x_train and y_train")
     print(X.shape, y.shape)
 
@@ -224,56 +127,266 @@ else:
     X_rest = X[: -test_size]
     y_rest = y[: -test_size]
 
-    X_train, X_valid, y_train, y_valid = train_test_split(X_rest, y_rest, test_size = 0.15, random_state = 101)
+    X_train, X_valid, y_train, y_valid = train_test_split(X_rest, y_rest, test_size=0.15, random_state=101)
+    predictedvalues = []
+    x_t = X_test[0]
 
-    X_train = X_train.reshape(X_train.shape[0], lookback, 5)
-    X_valid = X_valid.reshape(X_valid.shape[0], lookback, 5)
-    X_test = X_test.reshape(X_test.shape[0], lookback, 5)
-
-
-    print("shapes x_train shape x_valid, X-test")
-    print(X_train.shape)
-    print(X_valid.shape)
-    print(X_test.shape)
-    print(X_train[1][:][:])
-    # Creating the architecture
-    regressor = Sequential()
-    # add 1st layer
-    regressor.add(LSTM(units = 512, return_sequences = True, input_shape = (X_train.shape[1], 5)))
-    regressor.add(Dropout(rate = 0.2))
-
-    # 2nd layer
-    regressor.add(LSTM(units = 512, return_sequences = True))
-    regressor.add(Dropout(rate = 0.2))
-
-    # 3rd layer
-    regressor.add(LSTM(units = 32, activation = 'relu', return_sequences = False))
-    regressor.add(Dropout(rate = 0.2))
-
-    # add output layer
-    regressor.add(Dense(units = 1))
-
-    # We introduc callbacks:
-    # EarlyStoping: It will stop the traning if score of model didn't increase.
-    # ReduceLROnPlateau: Use for reduce the learning rate.
-    # ModelCheckpoint: Use for save model only when the score increased
-    callbacks = [
-    EarlyStopping(patience=10, verbose=1),
-    ReduceLROnPlateau(factor=0.1, patience=3, min_lr=0.00001, verbose=1),
-    ModelCheckpoint('model.h5', verbose=1, save_best_only=True, save_weights_only=True)
-    ]
-
-    # regressor.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
-    regressor.compile(optimizer='adam', loss='mean_squared_error', metrics=[soft_acc])
-    regressor.fit(X_train, y_train, epochs=60, batch_size=32, validation_data=(X_valid, y_valid), callbacks=callbacks)
-    predicted_value = regressor.predict(X_test)
-
+    print("shape" , X_test.shape)
+    x_t = x_t.reshape(1, lookback, 1)
+    print("shape", x_t.shape)
+    startp = 0
+    predictlen = 20
+    X_test = X_test[startp:startp+predictlen]
+    Y_test = Y_test[startp:startp+predictlen]
+    for i in range(predictlen):
+        predicted_value = regressor.predict(x_t)
+        predictedvalues.append(predicted_value[0])
+        x_new = x_t
+        x_new[0][lookback-1] = predicted_value
+        x_new[0][0:lookback-1] = x_t[0][1:]
+    print(predictedvalues)
     plt.figure(figsize=(18, 8))
-    plt.plot(predicted_value, label = 'Predicted Value')
-    plt.plot(Y_test, label = 'Real Value')
+    plt.plot(predictedvalues, label='Predicted Value')
+    plt.plot(Y_test, label='Real Value')
     plt.legend(loc='upper left')
     plt.title("Close Value of S&P 500")
     plt.xlabel("Days")
     plt.ylabel("Close Value")
     plt.show()
+
+
+
+
+
+if training ==True:
+
+
+
+
+
+
+
+
+
+
+    if oneFeature:
+
+
+        #keras only takes numpy array
+
+        #scaler = MinMaxScaler(feature_range = (0, 1))
+        #fit: get min/max of train data
+        #training_set_scaled = scaler.fit_transform(training_set)
+        input_feature = dataset_train[['Close']]
+        input_data = input_feature.values
+        print("type", type(input_data))
+        # scaler = MinMaxScaler(feature_range=(0, 1))
+        # input_data[:,:] = scaler.fit_transform(input_data[:,:])
+
+        # input_data = scaler.fit_transform(input_data)
+        # Normalize the data
+        inputnew = []
+        print("going to scale")
+        print("shape input", input_data.shape)
+        katt = 0
+        rangestuff = 8
+        print(len(input_data))
+        a = np.zeros((len(input_data), 1))
+        for i in range(rangestuff):
+            scaler = MinMaxScaler(feature_range=(0, 1))
+            # input_data[:,:] = scaler.fit_transform(input_data[:,:])
+
+            t = scaler.fit_transform(
+                input_data[int(i * len(input_data) / rangestuff):int((i + 1) * len(input_data) / rangestuff), :])
+            a[int(i * len(input_data) / rangestuff):int((i + 1) * len(input_data) / rangestuff), :] = t
+
+            inputnew.append(t)
+
+        input_data = a
+
+        X = []
+        y = []
+        lookback =30
+        total_size = len(dataset_train)
+        print("shape input after", input_data.shape)
+        for i in range(0, total_size - lookback):  # loop data set with margin 50 as we use 50 days data for prediction
+            t = []
+            for j in range(0, lookback):  # loop for 50 days
+                current_index = i + j
+                t.append(input_data[current_index, :])  # get data margin from 50 days with marging i
+
+            # X 12580x 30x 5
+            # t 30 x 5
+
+            X.append(t)
+
+            y.append(input_data[lookback + i, 0])
+
+        X, y = np.array(X), np.array(y)
+        print("shapes x_train and y_train")
+        print(X.shape, y.shape)
+
+        test_size = 600
+
+        X_test = X[-test_size:]
+        Y_test = y[-test_size:]
+
+        X_rest = X[: -test_size]
+        y_rest = y[: -test_size]
+
+        X_train, X_valid, y_train, y_valid = train_test_split(X_rest, y_rest, test_size=0.15, random_state=101)
+
+
+        regressor = Sequential()
+        # add 1st lstm layer
+        regressor.add(LSTM(units=50, return_sequences=True, input_shape=(X_train.shape[1], 1)))
+        regressor.add(Dropout(rate=0.2))
+
+        ##add 2nd lstm layer: 50 neurons
+        regressor.add(LSTM(units=50, return_sequences=True))
+        regressor.add(Dropout(rate=0.2))
+
+        ##add 3rd lstm layer
+        regressor.add(LSTM(units=50, return_sequences=True))
+        regressor.add(Dropout(rate=0.2))
+
+        ##add 4th lstm layer
+        regressor.add(LSTM(units=50, return_sequences=False))
+        regressor.add(Dropout(rate=0.2))
+
+        ##add output layer
+        regressor.add(Dense(units=1))
+
+        regressor.compile(optimizer='adam', loss='mean_squared_error')
+        regressor.fit(x=X_train, y=y_train, batch_size=32, epochs=100)
+
+
+        predicted_value = regressor.predict(X_test)
+        regressor.save("modeljkdnsjkfsdjnk.h5")
+        plt.figure(figsize=(18, 8))
+        plt.plot(predicted_value, label = 'Predicted Value')
+        plt.plot(Y_test, label = 'Real Value')
+        plt.legend(loc='upper left')
+        plt.title("Close Value of S&P 500")
+        plt.xlabel("Days")
+        plt.ylabel("Close Value")
+        plt.show()
+
+
+
+
+    else:
+        # Selecting 5 interesting feature
+        input_feature = dataset_train[['Open', 'High', 'Low', 'Volume', 'Close']]
+        input_data = input_feature.values
+        print("type", type(input_data))
+        #scaler = MinMaxScaler(feature_range=(0, 1))
+        # input_data[:,:] = scaler.fit_transform(input_data[:,:])
+
+        #input_data = scaler.fit_transform(input_data)
+        # Normalize the data
+        inputnew = []
+        print("going to scale")
+        print("shape input" , input_data.shape)
+        katt = 0
+        rangestuff =8
+        a = np.zeros((int((rangestuff+1)*len(input_data)/rangestuff),5))
+        for i in range(rangestuff):
+
+            scaler = MinMaxScaler(feature_range=(0,1))
+            # input_data[:,:] = scaler.fit_transform(input_data[:,:])
+
+            t = scaler.fit_transform(input_data[int(i*len(input_data)/rangestuff):int((i+1)*len(input_data)/rangestuff),:])
+            a[int(i*len(input_data)/rangestuff):int((i+1)*len(input_data)/rangestuff),:] = t
+
+            inputnew.append(t)
+
+        input_data = a
+
+
+
+        lookback = 30
+        total_size = len(dataset_train)
+
+        X=[]
+        y=[]
+        print("shape input after" , input_data.shape)
+        for i in range(0, total_size-lookback): # loop data set with margin 50 as we use 50 days data for prediction
+            t=[]
+            for j in range(0, lookback): # loop for 50 days
+                current_index = i+j
+                t.append(input_data[current_index, :]) # get data margin from 50 days with marging i
+
+            # X 12580x 30x 5
+            # t 30 x 5
+
+            X.append(t)
+
+
+            y.append(input_data[lookback+i, 4])
+
+        X, y= np.array(X), np.array(y)
+        print("shapes x_train and y_train")
+        print(X.shape, y.shape)
+
+        test_size = 600
+
+        X_test = X[-test_size:]
+        Y_test = y[-test_size:]
+
+        X_rest = X[: -test_size]
+        y_rest = y[: -test_size]
+
+        X_train, X_valid, y_train, y_valid = train_test_split(X_rest, y_rest, test_size = 0.15, random_state = 101)
+
+        X_train = X_train.reshape(X_train.shape[0], lookback, 1)
+        X_valid = X_valid.reshape(X_valid.shape[0], lookback, 1)
+        X_test = X_test.reshape(X_test.shape[0], lookback, 1)
+
+
+        print("shapes x_train shape x_valid, X-test")
+        print(X_train.shape)
+        print(X_valid.shape)
+        print(X_test.shape)
+        print(X_train[1][:][:])
+        # Creating the architecture
+        regressor = Sequential()
+        # add 1st layer
+        regressor.add(LSTM(units = 512, return_sequences = True, input_shape = (X_train.shape[1], 5)))
+        regressor.add(Dropout(rate = 0.2))
+
+        # 2nd layer
+        regressor.add(LSTM(units = 512, return_sequences = True))
+        regressor.add(Dropout(rate = 0.2))
+
+        # 3rd layer
+        regressor.add(LSTM(units = 32, activation = 'relu', return_sequences = False))
+        regressor.add(Dropout(rate = 0.2))
+
+        # add output layer
+        regressor.add(Dense(units = 1))
+
+        # We introduc callbacks:
+        # EarlyStoping: It will stop the traning if score of model didn't increase.
+        # ReduceLROnPlateau: Use for reduce the learning rate.
+        # ModelCheckpoint: Use for save model only when the score increased
+        callbacks = [
+        EarlyStopping(patience=10, verbose=1),
+        ReduceLROnPlateau(factor=0.1, patience=3, min_lr=0.00001, verbose=1),
+        ModelCheckpoint('model.h5', verbose=1, save_best_only=True, save_weights_only=True)
+        ]
+
+        # regressor.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
+        regressor.compile(optimizer='adam', loss='mean_squared_error', metrics=[soft_acc])
+        regressor.fit(X_train, y_train, epochs=60, batch_size=32, validation_data=(X_valid, y_valid), callbacks=callbacks)
+        predicted_value = regressor.predict(X_test)
+
+        plt.figure(figsize=(18, 8))
+        plt.plot(predicted_value, label = 'Predicted Value')
+        plt.plot(Y_test, label = 'Real Value')
+        plt.legend(loc='upper left')
+        plt.title("Close Value of S&P 500")
+        plt.xlabel("Days")
+        plt.ylabel("Close Value")
+        plt.show()
 
